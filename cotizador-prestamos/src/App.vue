b7f6c5eecc853b4ad45fdb009b3870e37024a6e5
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-06-18 21:34:50 -0600

    primer componente

--- cotizador-prestamos/src/App.vue
+++ cotizador-prestamos/src/App.vue
@@ -1,5 +1,33 @@
-<script setup></script>
+<script setup>
+import { ref, reactive } from "vue";
+import Header from "./components/Header.vue";
+
+const cantidad = ref(0);
+
+const state = reactive({
+  cantidad: 0,
+});
+
+function handleChange(e) {
+  cantidad.value = +e.target.value;
+}
+
+console.log(cantidad.value);
+console.log(state.cantidad);
+</script>
 
 <template>
-  <h1>Hola mundo</h1>
+  <div class="my-20 max-w-lg mx-auto bg-white shadow p-10">
+    <Header />
+
+    <div class="my-5">
+      <input
+        type="range"
+        class="w-full bg-gray-200 accent-lime-500 hover:accent-lime-600"
+        @input="handleChange"
+      />
+
+      {{ cantidad }}
+    </div>
+  </div>
 </template>
