4710a8d2563bbca765e16901f89fbd5ed267242b
unknown <hernandezgalvezalejandro@gmail.com>
2024-03-14 16:32:33 -0600

    carpeta de componentes

---
+++ curso/render/src/components/Usuario.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Usuario = () => {
+  return (
+    <div>
+      <h1>Hola {nombre}</h1>
+      <p>Tu lista de amigos es</p>
+    </div>
+  )
+}
+
+export default Usuario
