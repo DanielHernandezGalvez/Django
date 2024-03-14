0c3ee65de9094f7daacfd8dd6a7f0f691fb64f64
unknown <hernandezgalvezalejandro@gmail.com>
2024-03-14 16:43:26 -0600

    multititulo!

--- curso/render/src/components/Usuario.jsx
+++ curso/render/src/components/Usuario.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 const Usuario = () => {
   return (
     <div>
-      <h1>Hola {nombre}</h1>
-      <p>Tu lista de amigos es</p>
+      <h1>Titulo {nombre}</h1>
     </div>
   )
 }
