58165501808b4e07ca3675bd25f5d7aeed992686
unknown <hernandezgalvezalejandro@gmail.com>
2024-03-19 21:11:47 -0600

    e.target.value

--- curso/render/src/components/FormularioInicioSesion.jsx
+++ curso/render/src/components/FormularioInicioSesion.jsx
@@ -1,12 +1,32 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 const FormularioInicioSesion = () => {
+const [user, setUser] = useState("")
+const [pass, setPass] = useState("")
+
   return (
     <form action=''>
-      <p>usuario:</p>
-      <p>contraseña:</p>
-      <label htmlFor="usuario">Usuario</label>
-      <input type="text" name="usuario" id="usuario" />
+      <p>usuario:  {user}</p>
+      <p>contraseña: {pass}</p>
+      <div>
+        <label htmlFor="usuario">Usuario</label>
+        <input 
+          type="text" 
+          name="usuario" 
+          id="usuario" 
+          value={user}
+          onChange={(e) => setUser(e.target.value)}
+          />
+        <label htmlFor="usuario">Contraseña</label>
+        <input 
+          type="text"  
+          name="password" 
+          id="password" 
+          value={pass}
+          onChange={(e) => setPass(e.target.value)}
+          />
+        <button type='submit'>Iniciar seseion</button>
+      </div>
     </form>
   )
 }
